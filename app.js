0273148777fc829980a54ceb7779a0cf854d2237
RoqaiaRF <qoraia97@gmail.com>
2022-03-30 19:29:39 +0300

    Change Structure & add demo

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var logger  = require('morgan'); // testtest test
+const bodyParser = require("body-parser");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var demoRouter = require('./routes/demo');
 
 var app = express();
         
-           
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/demo', demoRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
+})
 
-module.exports = app;
