d9a14d053e07999b144b8d02e8199efc5746b59f
RoqaiaRF <qoraia97@gmail.com>
2022-04-16 14:35:02 +0300

    fix bugs

--- database/connection.js
+++ database/connection.js
@@ -4,14 +4,13 @@ require("dotenv").config();
 const DB_DATABASE = process.env.DB_DATABASE;
 const DB_USERNAMES = process.env.DB_USERNAMES;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DIALECT = process.env.DIALECT;
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAMES, DB_PASSWORD, {
     host: HOST,
-    dialect: DIALECT,
+    dialect: "mysql",
     port: PORT
 });
 
