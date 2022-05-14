a951dbf2be80d77e94acf7c4c53495e7b4f17ce6
RoqaiaRF <qoraia97@gmail.com>
2022-05-14 09:17:42 +0300

    fix phase 3.1

--- app/controllers/isReservation_OrdersController.js
+++ app/controllers/isReservation_OrdersController.js
@@ -1,20 +1,7 @@
 
 
 const isReservation_Pay = (storObj) => {
- /*
-    const reservations = storObj.reservations_policy
-    const pay1 = storObj.pay_when_receiving
-    const pay2 = storObj.pay_after_receiving
-    //اذا كان المتجر يستخدم سياسة الطلب ارجع 1 واذا كان يستخدم سياسة الحجز ارجع 0
-    if ((pay1 || pay2 ) && ( !reservations )) { // سياسة دفع فقط
-      return "onlyPay"
-    }
-    else if ((!pay1 && !pay2) && (reservations)) {
-      return "onlyReservation" 
-     }
-    else { return "Pay_Reservation_together" }
-
-*/
+ 
 const is_order = storObj.is_order
 const is_reservation = storObj.is_reservation
 
