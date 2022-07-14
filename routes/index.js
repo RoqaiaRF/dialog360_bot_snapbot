d4af8a6e0fc189eae3ad21c7c96fe15d222e3f6e
sharaf eldeen <eldeenshraf@gmail.com>
2022-07-14 15:37:59 +0200

    fix router problem

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post("/", function (req, res, next) {
 
   let message = req.body.Body; // text message sent
   let sender_ID = req.body.From; // End-User Phone number
-  let receiver_id = req.body.WaId; // store owner Phone number
+  let receiver_id = req.body.To; // store owner Phone number
   let longitude = req.body.Longitude;
   let latitude = req.body.Latitude;
   let username = req.body.ProfileName;
