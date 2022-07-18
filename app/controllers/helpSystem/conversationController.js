f438c21d348bd296411360d412c1c262b143f4b6
sharaf eldeen <eldeenshraf@gmail.com>
2022-07-18 19:53:23 +0200

    edit endpoints

--- app/controllers/helpSystem/conversationController.js
+++ app/controllers/helpSystem/conversationController.js
@@ -14,41 +14,86 @@ const findConversations = async (req, res) => {
   const limit = per_page;
   const data = await Conversations.findAll({
     where: { number_store: phone },
-    include: { 
+    include: {
       model: Messages,
-   
+      limit: 50,
+      offset: 0,
+      order: ["createdAt"],
     },
-
+    order: ["createdAt"],
   });
   if (data.length == 0) return res.status(204).json({ msg: "success", data });
-  return res.status(200).json({ msg: "success", data });
+  return res.status(200).json({
+    msg: "success",
+    data: data.map((conv) => ({
+      ...conv.dataValues,
+      lastMessage: conv.dataValues.messages[0],
+    })),
+  });
 };
 
-const findMessages = async(req, res) => {
+const findMessages = async (req, res) => {
   const { store } = req;
   const { page = 1 } = req.params;
-  const { per_page = 12 } = req.params;
+  const { per_page = 30 } = req.params;
   const { id } = req.params;
   const { phone } = store;
   const offset = (page - 1) * per_page;
   const limit = per_page;
-
-  const data = await Conversations.findOne({
+  const[conversation, count] = await Promise.all([Conversations.findOne({
     where: {
       id,
       number_store: phone,
     },
-    include:{
-        model:Messages,
+    include: {
+      model: Messages,
+      limit,
+      offset,
+      order: ["createdAt"],
+    },
+
+    order: ["createdAt"],
+  }),
+  Messages.count({where:{conversation_id:id}})]
+  )
+  const last_page = Math.ceil(count/per_page);
+  if (!conversation)
+    return res.status(404).json({ msg: "not found", err: "not found" });
+  const data = {...conversation.dataValues,last_page, current_page:page, per_page }
+    return res.status(200).json({ msg: "success", data });
+};
+
+const storeMessage = async (req, res) => {
+  const { id } = req.params;
+  const { store } = req;
+  const { phone } = store;
+  const { message } = req.body;
   
-    } 
+  if (!message)
+    return res
+      .status(400)
+      .json({
+        msg: "bad request",
+        err: { message: "you should provide message" },
+      });
+
+  const conversation = await Conversations.findOne({
+    where: { id, number_store: phone },
+    attributes: ["id"],
   });
-  if(!data)
-    return res.status(404).json({msg:'not found', err:'not found'});
-  return res.status(200).json({msg:'success', data})
+  if (!conversation)
+    return res.status(404).json({ msg: "not found", err: "not found" });
+
+  await Messages.create({
+    message,
+    conversation_id: id,
+    sender_number: phone,
+  });
+  return res.status(200).json({ msg: "success" });
 };
 
 module.exports = {
-    findConversations,
-    findMessages
-}
+  findConversations,
+  findMessages,
+  storeMessage,
+};
--- routes/conversationRouter.js
+++ routes/conversationRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ConversationController = require("../app/controllers/helpSystem/conversationController");
-const { findConversations, findMessages } = ConversationController;
+const { findConversations, findMessages, storeMessage } = ConversationController;
 router.get("/", findConversations);
 router.get('/:id', findMessages)
-
+router.put('/:id', storeMessage)
 module.exports = router
