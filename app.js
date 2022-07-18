0b90ac25bf284a4dbaa20b959e2f45cca1ff9a62
RoqaiaRF <qoraia97@gmail.com>
2022-07-18 14:53:15 +0300

    add header to cors

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var cors = require('cors');
     cors({
       allowedHeaders: ["authorization", "Content-Type", 'X-Requested-With', "Origin", "Accept"], // you can change the headers
       exposedHeaders: ["authorization", 'X-Requested-With'], // you can change the headers
-      origin: "*",
+      origin: "https://store.snapbot.app",
+      host: "https://bot.snapbot.app",
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      preflightContinue: false
+      preflightContinue: false,
+      
     })
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
+
   });
 
   
